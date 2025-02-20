a1d75ba956f381e7daaa71742f1ff539f3684468
hawsatit <hawsatit@kilby.cs.grinnell.edu>
2025-02-20 13:38:56 -0600

    Finished the implementations for both lists

--- src/main/java/edu/grinnell/csc207/listlab/LinkedList.java
+++ src/main/java/edu/grinnell/csc207/listlab/LinkedList.java
@@ -4,20 +4,48 @@ package edu.grinnell.csc207.listlab;
  * A linked implementation of the list ADT.
  */
 public class LinkedList {
+    
+    private static class Node{
+        public int value;
+        public Node next;
+        
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+    
+    private Node first;
+    private int size;
+    
+    public LinkedList(){
+        this.first = null;
+    }
+    
+    
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (first == null){
+            first = new Node(value, null);
+        } else {
+            Node cur = this.first;
+            while (cur.next != null){
+                cur = cur.next;
+            }
+            cur.next = new Node(value, null);    
+        }
+        this.size++;
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return this.size;
     }
 
     /**
@@ -25,7 +53,13 @@ public class LinkedList {
      * @return the value at the specified <code>index</code>
      */
     public int get(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        Node cur = this.first;
+        int i = 0;
+        while (cur.next != null && i <= index){
+            cur = cur.next;
+            i++;
+        }
+        return cur.value;
     }
 
     /**
@@ -35,6 +69,31 @@ public class LinkedList {
      * @return the element at <code>index</code>
      */
     public int remove(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        if (first == null){
+            throw new IndexOutOfBoundsException("the list is empty");
+        }
+        if (index == 0){
+            int value =  first.value;
+            first = first.next;
+            size--;
+            return value;
+        } else {
+            Node cur = this.first;
+            int i = 0;
+            while (cur != null && i < index - 1){
+                cur = cur.next;
+                i++;
+            }
+                
+            if (cur.next == null || cur == null){
+                throw new IndexOutOfBoundsException("Index is out of bounds");
+            }
+                
+            int value = cur.next.value;
+            cur.next = cur.next.next;
+            this.size--;
+            return value;
+        }
+           
     }
 }
