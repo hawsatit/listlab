f4c8b2037e683910f314735d806e4f9c5f412cf2
hawsatit <hawsatit@kilby.cs.grinnell.edu>
2025-02-20 14:10:53 -0600

    Finished the implementations for both lists

--- src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
@@ -1,23 +1,36 @@
 package edu.grinnell.csc207.listlab;
-
+import java.util.Arrays;
 /**
  * An array-based implementation of the list ADT.
  */
 public class ArrayList {
+    
+    private int size;
+    private int[] data;
+    
+    private static final int INITIAL_SIZE = 8;
+    
+    public ArrayList(){
+        this.data = new int[INITIAL_SIZE];
+        this.size = 0;
+    }
+    
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        grow();
+        this.data[size] = value;
+        this.size = this.size + 1;   
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return this.size;
     }
 
     /**
@@ -25,7 +38,13 @@ public class ArrayList {
      * @return the value at the specified <code>index</code>
      */
     public int get(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        return this.data[index];
+    }
+    
+    public void grow(){
+        if (size == data.length){
+            Arrays.copyOf(data, data.length *2);
+        }
     }
 
     /**
@@ -35,6 +54,11 @@ public class ArrayList {
      * @return the element at <code>index</code>
      */
     public int remove(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        int val = this.data[index];
+       for(int i = index; i < this.size; i++){
+           this.data[i] = this.data[i+1];
+       }
+       this.size--;
+       return val;
     }
 }
